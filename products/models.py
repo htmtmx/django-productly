8223883d61e1a97fd95165ff620931ea6ba26282
htmtmx <cesar.pineda@daksha.mx>
2023-09-13 12:56:15 -0600

    04 - Add str to models, custom visualization of models in django admin panel

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+    
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +14,7 @@ class Product(models.Model):
     score = models.FloatField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
