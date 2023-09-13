7123a3eaedf0112a7cb4f10a10e0543296602976
htmtmx <cesar.pineda@daksha.mx>
2023-09-13 13:08:35 -0600

    05 - Get data from DB, return like HttpReponse or JsonResponse

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world!!!")
+    products = Product.objects.all().values()
+    print(products)
+    # return HttpResponse(products[0].name + " " + str(products[0].created_at))
+    return JsonResponse(list(products), safe=False)
